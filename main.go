38e4241148f5308a0d5fa35b20a5e54899f42592
Darren Lau <panyuenlau@gmail.com>
2020-10-20 15:43:05 -0400

    udpate main code logic and docker container timezone

--- main.go
+++ main.go
@@ -9,27 +9,51 @@ import (
 	"google.golang.org/grpc"
 )
 
-const address = "localhost:50051"
+const (
+	// address for local debug
+	address = "localhost:50051"
+	// address used in the K8s cluster
+	// address = "grpc-service:50051"
+
+	connectionDeadline = 5 // max amount of time the client tries to build connection with the server
+)
+
+/*
+1. If the client cannot connect, logs error, return
+2. If the client connects, but the server doesn't respond before timeout, logs error, return
+*/
 
 func main() {
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	log.Println("Connecting to server...")
+
+	// set timeout for dialing to the server
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Duration(connectionDeadline)*time.Second))
+
 	if err != nil {
 		log.Fatalf("ERROR: failed to connect: %v", err)
+		return
 	}
+
+	log.Println("Server connected!")
+
 	defer conn.Close() // defer the execution of a function until the surrounding functions return
-	c := pb.NewGreetingClient(conn)
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	for {
+		c := pb.NewGreetingClient(conn)
 
-	sendMsg := "Hello Server!"
-	r, err := c.SayHello(ctx, &pb.Request{ReqeustMessage: sendMsg})
+		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+		defer cancel()
 
-	if err != nil {
-		log.Fatalf("ERROR: failed to send message to the server: %v", err)
-	} else {
-		log.Printf("The message \"" + sendMsg + "\"" + " was sent to the server, waiting for response...")
+		sendMsg := "Hello Server! " + time.Now().Format("2006-01-02 15:04:05 PM")
+		r, err := c.SayHello(ctx, &pb.Request{ReqeustMessage: sendMsg})
+
+		if err != nil {
+			log.Fatalf("ERROR: failed to send message to the server: %v", err)
+			return
+		}
+		log.Printf("The message \"%s\" has been sent to the server", sendMsg)
+
+		log.Printf("Respopnse from the server: \"%s\"\n\n", r.GetReplyMessage())
+		time.Sleep(time.Second * 5)
 	}
-
-	log.Printf("Message from the server: \"%s\n", r.GetReplyMessage()+"\"")
 }
