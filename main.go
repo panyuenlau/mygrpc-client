59dadbe8dcc75a48e68f6fd9178e5fc12bf255e0
Darren Lau <panyuenlau@gmail.com>
2020-10-23 17:50:46 -0400

    add http error code

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"time"
+
+	"net/http"
 
 	pb "github.com/panyuenlau/mygrpc-client/proto"
 	"google.golang.org/grpc"
@@ -11,9 +14,9 @@ import (
 
 const (
 	// address for local debug
-	address = "localhost:50051"
+	// address = "localhost:50051"
 	// address used in the K8s cluster
-	// address = "grpc-service:50051"
+	address = "grpc-service:50051"
 
 	connectionDeadline = 5 // max amount of time the client tries to build connection with the server
 )
@@ -23,37 +26,42 @@ const (
 2. If the client connects, but the server doesn't respond before timeout, logs error, return
 */
 
-func main() {
-	log.Println("Connecting to server...")
+func handler(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("Check server status request received, now dialing the gRPC server....")
 
 	// set timeout for dialing to the server
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(time.Duration(connectionDeadline)*time.Second))
 
 	if err != nil {
-		log.Fatalf("ERROR: failed to connect: %v", err)
+		log.Fatalf("ERROR: failed to connect gRPC server: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	log.Println("Server connected!")
+	log.Println("gRPC server connected!")
 
 	defer conn.Close() // defer the execution of a function until the surrounding functions return
 
-	for {
-		c := pb.NewGreetingClient(conn)
+	c := pb.NewGreetingClient(conn)
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-		defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
 
-		sendMsg := "Hello Server! " + time.Now().Format("2006-01-02 15:04:05 PM")
-		r, err := c.SayHello(ctx, &pb.Request{ReqeustMessage: sendMsg})
+	sendMsg := "Hello Server! " + time.Now().Format("2006-01-02 15:04:05 PM")
+	r, err := c.SayHello(ctx, &pb.Request{ReqeustMessage: sendMsg})
 
-		if err != nil {
-			log.Fatalf("ERROR: failed to send message to the server: %v", err)
-			return
-		}
-		log.Printf("The message \"%s\" has been sent to the server", sendMsg)
-
-		log.Printf("Respopnse from the server: \"%s\"\n\n", r.GetReplyMessage())
-		time.Sleep(time.Second * 5)
+	if err != nil {
+		log.Fatalf("ERROR: failed to send message to the server: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	log.Printf("The message \"%s\" has been sent to the server", sendMsg)
+
+	log.Printf("Respopnse from the server: \"%s\"\n\n", r.GetReplyMessage())
+}
+
+func main() {
+	http.HandleFunc("/serverstatus", handler)
+	http.ListenAndServe(":8081", nil)
 }
